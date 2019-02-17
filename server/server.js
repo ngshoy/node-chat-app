6aff3a8af84e6e85964aa1176808f2be8012cbe7
Jack Zhang <ngshoy@hotmail.com>
2019-02-17 12:38:08 -0500

    add http server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3002;
 const publicPath = path.join(__dirname + '/../public');
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
