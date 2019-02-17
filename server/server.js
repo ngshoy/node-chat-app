9a97df0b2fbd1f223dcc41dae1c61b098a8ec483
Jack Zhang <ngshoy@hotmail.com>
2019-02-17 12:51:02 -0500

    added socket io setup to establish ws connection

--- server/server.js
+++ server/server.js
@@ -7,8 +7,17 @@ const PORT = process.env.PORT || 3002;
 const publicPath = path.join(__dirname + '/../public');
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', socket => {
+  console.log('a client has connected');
+
+  socket.on('disconnect', () => {
+    console.log('a client has disconnected');
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
