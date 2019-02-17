1917ec8c54e2ccd54a0a90771c73f52b031e08c5
Jack Zhang <ngshoy@hotmail.com>
2019-02-16 21:48:41 -0500

    set up express server to serve static html

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log(__dirname + '/../public');
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3002;
+const publicPath = path.join(__dirname + '/../public');
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
