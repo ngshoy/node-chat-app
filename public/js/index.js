95879efd8ba19985719bca5c1b20cba16b83ecea
Jack Zhang <ngshoy@hotmail.com>
2019-02-20 14:06:56 -0500

    add message list

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,10 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', message => {
   console.log('newMessage', message);
+  let li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 socket.emit('createMessage', {
@@ -21,11 +25,11 @@ socket.emit('createMessage', {
 
 jQuery('#message-form').on('submit', e => {
   e.preventDefault();
-   
+
   socket.emit('createMessage', {
     from: 'User',
-   text: jQuery('[name=message]').val()
+    text: jQuery('[name=message]').val()
   }, () => {
-      
+
   })
 });
