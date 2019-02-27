95c1eda084be8c5554cf0182d5d2f4c535fb4d91
Jack Zhang <ngshoy@hotmail.com>
2019-02-26 21:05:22 -0500

    add ux for send location button

--- public/js/index.js
+++ public/js/index.js
@@ -50,12 +50,16 @@ locationButton.on('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(position => {
+    locationButton.removeAttr('disabled').text('Send location'); 
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, () => {
+    locationButton.removeAttr('disabled').text('Send location'); 
     alert('Unable to fetch location.');
   });
 });
